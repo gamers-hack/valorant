e5e92c40d81e78eed28648bad80e50b93558ff26
kumimaru <harapx2002.blankr@gmail.com>
2025-03-17 01:18:30 +0900

    chore: パッケージ更新と各種設定ファイルの整備

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
+// https://vite.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "@/assets/style/variables" as *;
-          @use "@/assets/style/mixins" as *;
-        `,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [vue()],
 });
