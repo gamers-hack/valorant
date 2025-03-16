9cf0ec9f6eca0fac21aabeb568326f7a1b01f48d
kumimaru <harapx2002.blankr@gmail.com>
2025-03-17 01:17:50 +0900

    feat: レイアウトとDefaultHeaderコンポーネントの追加

---
+++ src/layouts/withHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <DefaultHeader />
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from '@/uiParts/DefaultHeader.vue';
+</script>
---
+++ src/uiParts/DefaultHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <p class="header__logo">ロゴ</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 10px 20px;
+  background-color: $color-background;
+
+  &__logo {
+    color: $color-white;
+  }
+}
+</style>
