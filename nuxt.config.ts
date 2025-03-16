cd96256b666a973a6f7e4bc36b7fca0821eaecf7
kumimaru <harapx2002.blankr@gmail.com>
2025-03-17 01:17:40 +0900

    config: Nuxt設定の強化（TypeScript厳格化、SCSS変数設定、SSR無効化）

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,56 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
+// import { NodePackageImporter } from 'sass-embedded';
 
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
   srcDir: 'src/',
   components: false,
   imports: {
-    autoImport: false,
+    autoImport: true,
+    dirs: [],
+  },
+  experimental: {
+    componentIslands: false,
+    typedPages: true,
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: _tag => false,
+      whitespace: 'preserve',
+    },
   },
   css: ['normalize.css', 'the-new-css-reset', '@/assets/style/global.scss'],
   typescript: {
     strict: true,
+    typeCheck: true,
+    shim: false,
     tsConfig: {
       compilerOptions: {
         paths: {
           '@': ['src'],
           '@/*': ['src/*'],
           '#hooks/*': ['../src/hooks/*'],
+        },
+        noImplicitAny: true,
+        strictNullChecks: true,
+        strictFunctionTypes: true,
+        strictBindCallApply: true,
+        strictPropertyInitialization: true,
+        noImplicitThis: true,
+        alwaysStrict: true,
+      },
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "sass:math"; @use "@/assets/style/_variables.scss" as *;',
+          api: 'modern-compiler',
         },
       },
     },
